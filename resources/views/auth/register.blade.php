ee065bd491c846a74e3618d873f3091950902371
winarno <narnowin195@gmail.com>
2022-09-11 09:11:21 +0700

    fix view login and register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
             <x-jet-authentication-card-logo />
@@ -57,4 +57,64 @@
             </div>
         </form>
     </x-jet-authentication-card>
+</x-guest-layout> --}}
+<x-guest-layout>
+    
+	<!--main area-->
+	<main id="main" class="main-site left-sidebar">
+
+		<div class="container">
+
+			<div class="wrap-breadcrumb">
+				<ul>
+					<li class="item-link"><a href="/" class="link">home</a></li>
+					<li class="item-link"><span>Register</span></li>
+				</ul>
+			</div>
+			<div class="row">
+				<div class="col-lg-6 col-sm-6 col-md-6 col-xs-12 col-md-offset-3">							
+					<div class=" main-content-area">
+                <x-jet-validation-errors class="mb-4" />
+						<div class="wrap-login-item ">
+							<div class="register-form form-item ">
+								<form class="form-stl" action="{{ route('register') }}" name="frm-login" method="post" >
+                                    @csrf
+                                    @method('POST')
+									<fieldset class="wrap-title">
+										<h3 class="form-title">Create an account</h3>
+										<h4 class="form-subtitle">Personal infomation</h4>
+									</fieldset>									
+									<fieldset class="wrap-input">
+										<label for="frm-reg-lname">Name*</label>
+										<input type="text" id="frm-reg-lname" name="name" placeholder="Your Name" :value="old('name')">
+									</fieldset>
+									<fieldset class="wrap-input">
+										<label for="frm-reg-email">Email Address*</label>
+										<input type="email" id="frm-reg-email" name="email" placeholder="Email address" :value="old('email')">
+									</fieldset>
+									<fieldset class="wrap-title">
+										<h3 class="form-title">Login Information</h3>
+									</fieldset>
+									<fieldset class="wrap-input item-width-in-half left-item ">
+										<label for="frm-reg-pass">Password *</label>
+										<input type="text" id="frm-reg-pass"  placeholder="Password" name="password" :value="old('password')">
+									</fieldset>
+									<fieldset class="wrap-input item-width-in-half ">
+										<label for="frm-reg-cfpass">Confirm Password *</label>
+										<input type="text" id="frm-reg-cfpass" name="password_confirmation" placeholder="Confirm Password" :value="old('Confirm Password')" >
+									</fieldset>
+									<input type="submit" class="btn btn-sign" value="Register" name="register">
+								</form>
+							</div>											
+						</div>
+					</div><!--end main products area-->		
+				</div>
+			</div><!--end row-->
+
+		</div><!--end container-->
+
+	</main>
+	<!--main area-->
+
 </x-guest-layout>
+
