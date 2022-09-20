e51597265f65a37a2b5b25328bd7a76387e0f94d
winarno <narnowin195@gmail.com>
2022-09-20 20:16:28 +0700

    update categories

--- app/Http/Livewire/Admin/AdminEditCategoryComponent.php
+++ app/Http/Livewire/Admin/AdminEditCategoryComponent.php
@@ -2,12 +2,35 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\Category;
 use Livewire\Component;
-
+use Illuminate\Support\Str;
 class AdminEditCategoryComponent extends Component
 {
+    public $category_slug;
+    public $category_id;
+    public $name;
+    public $slug;
+
+    public function mount($category_slug){
+        $this->category_slug = $category_slug;
+        $category=Category::where('slug',$category_slug)->first();
+        $this->category_id = $category->id;
+        $this->name = $category->name;
+        $this->slug = $category->slug;
+    }
+    public function generateslug(){
+        $this->slug = Str::slug($this->title);
+    }
+    public function updateCategory(){
+        $category=Category::find($this->category_id);
+        $category->name=$this->name;
+        $category->slug=$this->slug;
+        $category->save();
+        session()->flash('message', 'Successfully updated category !');
+    }
     public function render()
     {
-        return view('livewire.admin.admin-edit-category-component');
+        return view('livewire.admin.admin-edit-category-component')->layout('Layouts.base');
     }
 }
